893bfc6145ae900f6dbd5e6db24d576d2c6ce0ab
tjdrb3807 <junsung3807@naver.com>
2023-01-14 20:59:55 +0900

    project 생성 및 git 설정

---
+++ src/test/java/SmartMetro/metro/MetroApplicationTests.java
@@ -0,0 +1,13 @@
+package SmartMetro.metro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
